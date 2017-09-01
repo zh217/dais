cb4aea10f0ca3b29bb3896896ef2bb21f791dfd7
Ziyang Hu <hu.ziyang@cantab.net>
2017-09-02 00:33:35 +0800

    supporting different keys for different batches

--- src/dais/graphql/dataloader.clj
+++ src/dais/graphql/dataloader.clj
@@ -10,9 +10,8 @@
   (:import (com.walmartlabs.lacinia.resolve ResolverResult)))
 
 (defn make-dataloader
-  [conn {:keys [key-fn key-processor]
-         :or   {key-fn        :id
-                key-processor identity}}]
+  [conn {:keys [key-processor]
+         :or   {key-processor identity}}]
   (let [dataloader-chan (a/chan 1024)
         put-sentinel #(a/>!! dataloader-chan ::sentinel)
         put-force-sentinel #(a/>!! dataloader-chan ::force-sentinel)
@@ -29,9 +28,8 @@
               (or (= data ::force-sentinel)
                   (and sentinel? (= data ::sentinel)))
               (do
-                ;; TODO support vector of keys
                 (trace "dataloader pending requests:" (count pending))
-                (doseq [[batch-fn vs] pending]
+                (doseq [[[batch-fn key-fn key-proc] vs] pending]
                   (h/with-conn [c conn]
                     (let [ks (set (map first vs))
                           result (try
@@ -40,19 +38,19 @@
                                      (error ex)))
                           result (into {}
                                        (for [row result]
-                                         [(key-fn row) row]))
+                                         [((or key-proc key-processor) (key-fn row)) row]))
                           missing (into {}
                                         (for [k (set/difference ks (set (keys result)))]
                                           [k ::not-found]))]
                       (when (seq result)
                         (trace "dataloader result" result)
-                        (swap! cache update batch-fn merge result))
+                        (swap! cache update [batch-fn key-fn key-proc] merge result))
                       (when (seq missing)
                         (trace "dataloader missing values" missing)
-                        (swap! cache update batch-fn merge missing))
+                        (swap! cache update [batch-fn key-fn key-proc] merge missing))
                       (trace "dataloader cache" @cache)
                       (doseq [[k c] vs]
-                        (let [v (get-in @cache [batch-fn k])]
+                        (let [v (get-in @cache [[batch-fn key-fn key-proc] k])]
                           (when-not (= v ::not-found)
                             (trace "dataloader putting new value" k v)
                             (a/put! c v)))
@@ -67,30 +65,24 @@
                 (recur pending true))
 
               :else
-              (let [{:keys [batch-fn ret-chan key]} data]
-                (if (sequential? key)
+              (let [{:keys [batch-fn key-fn key-proc ret-chan key]} data]
+                (if-let [cache-value (get-in @cache [[batch-fn key-fn key-proc] ((or key-proc key-processor) key)])]
+                  ;; third case: incoming request has applicable cache
                   (do
-                    ;; TODO support vector of keys
+                    (when-not (= cache-value ::not-found)
+                      (trace "dataloader putting cached value" key cache-value)
+                      (a/put! ret-chan cache-value))
+                    (a/close! ret-chan)
+                    (put-sentinel)
+                    (recur pending false))
+                  ;; fourth case: incoming request does not have applicable cache
+                  (do
+                    (if (> (count (mapcat second pending)) 512)
+                      (put-force-sentinel)
+                      (put-sentinel))
                     (recur
-                      (identity pending)
-                      false))
-                  (if-let [cache-value (get-in @cache [batch-fn (key-processor key)])]
-                    ;; third case: incoming request has applicable cache
-                    (do
-                      (when-not (= cache-value ::not-found)
-                        (trace "dataloader putting cached value" key cache-value)
-                        (a/put! ret-chan cache-value))
-                      (a/close! ret-chan)
-                      (put-sentinel)
-                      (recur pending false))
-                    ;; fourth case: incoming request does not have applicable cache
-                    (do
-                      (if (> (count (mapcat second pending)) 512)
-                        (put-force-sentinel)
-                        (put-sentinel))
-                      (recur
-                        (update pending batch-fn conj [(key-processor key) ret-chan])
-                        false)))))))
+                      (update pending [batch-fn key-fn key-proc] conj [((or key-proc key-processor) key) ret-chan])
+                      false))))))
           ;; dataloader is closed
           (do
             (trace "dataloader cache at closing" @cache)
@@ -100,7 +92,7 @@
     dataloader-chan))
 
 (defn batch-loader
-  [{:keys [batch value-> arg-> extract-fn]}]
+  [{:keys [batch value-> arg-> extract-fn key-fn key-proc] :or {key-fn :id}}]
   (trace "Making dataloader with" batch value-> arg->)
   ^ResolverResult ^:graphql/no-wrap
   (fn [{:keys [dataloader] :as ctx} args value]
@@ -110,16 +102,19 @@
                      arg-> (arg-> args)
                      extract-fn (extract-fn ctx args value))]
         (let [selections (executor/selections-seq ctx)]
-          (trace "Dataloader Batch" key (vec selections))
-          (if (and (= 1 (count selections))
-                   (= "id" (name (first selections)))
+          (trace "Dataloader Batch" key key-fn (vec selections))
+          (if (and (keyword? key-fn)
+                   (= 1 (count selections))
+                   (= (name key-fn) (name (first selections)))
                    (nil? value))
-            (resolve/resolve-as {:id key})
+            (resolve/resolve-as {key-fn key})
             (let [resolve-promise (resolve/resolve-promise)
                   ret-chan (a/promise-chan)
                   payload {:batch-fn batch
                            :ret-chan ret-chan
-                           :key      key}]
+                           :key      key
+                           :key-fn   key-fn
+                           :key-proc key-proc}]
               (trace "Dataloader is called with payload" payload)
               (a/put! dataloader payload)
               (a/take! ret-chan
