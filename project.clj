28a6fb7c42dc79b13b98ed794e9944cde3880ef2
Ziyang Hu <hu.ziyang@cantab.net>
2017-09-05 21:50:48 +0800

    non-snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject infihis/dais "0.1.0-SNAPSHOT"
+(defproject infihis/dais "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
