6195e52033c854277d1a44a0fd6cb7b7e030cfe5
Ziyang Hu <hu.ziyang@cantab.net>
2017-09-10 13:01:35 +0800

    bump deps

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                  [cheshire "5.8.0"]
                  [cc.qbits/spandex "0.5.2" :exclusions [org.clojure/clojure]]
                  [org.clojure/java.jdbc "0.7.1"]
-                 [org.clojure/tools.reader "1.0.6"]
+                 [org.clojure/tools.reader "1.1.0"]
                  [me.raynes/conch "0.8.0"]
                  [aysylu/loom "1.0.0" :exclusions [tailrecursion/cljs-priority-map]]
                  [instaparse "1.4.7"]
